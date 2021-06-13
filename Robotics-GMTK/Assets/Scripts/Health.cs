696230ba0394b446157e5d97f79350fd8f74a637
LauriceThomas <49695257+LauriceThomas@users.noreply.github.com>
2021-06-12 19:30:58 -0500

    Added Health Script & art/sfx files
    Health script offers TakeDamage, GainHealth, AdjustHPBar.
    And requires a health bar image.
    
    Also added sfx files to folder & some art that has pixel art settings.

---
+++ Robotics-GMTK/Assets/Scripts/Health.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Health : MonoBehaviour
+{
+    public float totalHP;
+    public float currentHP;
+    public float barFillPercent;
+    public float barFillTotal;
+    public bool isDead;
+    public Image HPBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = totalHP;
+        isDead = false;
+        barFillPercent = barFillTotal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*Health bar test
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            GainHealth(5);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TakeDamage(5);
+        }*/
+    }
+
+    public void AdjustHPBar()
+    {
+       barFillPercent = currentHP / totalHP;
+
+       HPBar.fillAmount = barFillPercent;
+    }
+
+    public void GainHealth(float recoveryPoints)
+    {
+        if((currentHP + recoveryPoints) < totalHP)
+        {
+            currentHP += recoveryPoints;
+        }
+        else
+        {
+            currentHP = totalHP;
+        }
+
+        AdjustHPBar();
+    }
+
+    public void TakeDamage(float damagePoints)
+    {
+        if ((currentHP -= damagePoints) <= 0)
+        {
+            isDead = true;
+        }
+        else
+        {
+            currentHP -= damagePoints;
+        }
+
+        AdjustHPBar();
+    }
+}
