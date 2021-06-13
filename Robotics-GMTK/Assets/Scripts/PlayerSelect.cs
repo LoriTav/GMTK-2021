c679607098b5f711ab4255b8286d22689a29f9d2
LauriceThomas <49695257+LauriceThomas@users.noreply.github.com>
2021-06-13 08:19:15 -0500

    Character Select & more art added

---
+++ Robotics-GMTK/Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerSelect : MonoBehaviour
+{
+    public Image Selector;
+    public Transform place1;
+    public Transform place2;
+    public Transform place3;
+    public Transform currentPlace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlace = place2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentPlace == place2)
+            {
+                Selector.transform.position = place3.position;
+                currentPlace = place3;
+            }
+            else if (currentPlace == place3)
+            {
+                Selector.transform.position = place1.position;
+                currentPlace = place1;
+            }
+            else
+            {
+                Selector.transform.position = place2.position;
+                currentPlace = place2;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentPlace == place2)
+            {
+                Selector.transform.position = place1.position;
+                currentPlace = place1;
+            }
+            else if (currentPlace == place3)
+            {
+                Selector.transform.position = place2.position;
+                currentPlace = place2;
+            }
+            else
+            {
+                Selector.transform.position = place3.position;
+                currentPlace = place3;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+}
